ddd0566e61bd86aa2db441d56bef10181556b532
Daniel Kitzmann <34245734+daniel-kitzmann@users.noreply.github.com>
2025-10-23 17:16:45 +0200

    Update bash file to download data from Zenodo

--- install_input_files.bash
+++ install_input_files.bash
@@ -1,2 +1,3 @@
 
-wget -r -np -nH -R "index.html*" --cut-dirs=3 -P input https://chaldene.unibe.ch/data/helios/input_files/
+wget -np -nH -O HELIOS_input_data.zip https://zenodo.org/records/17425932/files/HELIOS_input_data.zip
+unzip HELIOS_input_data.zip
